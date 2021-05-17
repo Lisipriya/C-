b1f22b82d12df8d6ba45ba3d31eca6b1c21f9793
Lisipriya <aplisipriya1998@gmail.com>
2021-05-17 19:01:28 +0530

    Basic CSharp

---
+++ Basics/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Overflow
+            Console.WriteLine("{0} {1}",byte.MinValue, byte.MaxValue);
+            byte number = 255;
+            number = (byte)(number + 1);
+            Console.WriteLine("number: " + number);
+            /*Exception will be thrown if the values exceeds the byte limit
+             * for the below commented lines*/
+            try
+            {
+                checked
+                {
+                    byte number1 = 255;
+                    number1 = (byte)(number1 + 1);
+                    Console.WriteLine("number: " + number1);
+                }
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("The Overflow exception occurs"); ;
+            }
+                    
+           
+            
+        }
+    }
+}
